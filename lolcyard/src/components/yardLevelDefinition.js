2e95a91b249e352e2d5f2d870379b195c486f5a6
student <student@localhost.localdomain>
2021-03-22 10:59:24 +0530

    add boostrap

---
+++ lolcyard/src/components/yardLevelDefinition.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { Button } from 'react-bootstrap';
+
+class YardLevelDefinition extends Component {
+    render() {
+        return (
+            <div>
+                <h2>thushan</h2>
+            </div>
+        );
+    }
+}
+
+export default YardLevelDefinition;
